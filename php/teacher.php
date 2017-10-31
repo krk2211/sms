09ce0d153d3bdc401851b23fd3a4135459d76c5b
Kiran Radhakrishnan <kiranrk2211@gmail.com>
2017-11-01 01:11:15 +0530

    Changed Title

--- php/teacher.php
+++ php/teacher.php
@@ -2,7 +2,7 @@
 
 <!DOCTYPE html>
 <html>
-<title>Attendance</title>
+<title>Teacher</title>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="../css/w3.css">
